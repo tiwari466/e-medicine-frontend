0fc12667fe0451318d872f0ac194f342462978b5
bmtiwari9450 <balmukund.tiwari@mydesq.com>
2026-06-20 22:39:59 +0530

    Updated API URL to production

--- src/config/env.js
+++ src/config/env.js
@@ -1,7 +1,15 @@
-export const API_BASE_URL =
+/*export const API_BASE_URL =
   "https://localhost:44302/api";
 
 export const ENV = {
   IMAGE_BASE_URL:
     "https://localhost:44302",
+};
+*/
+export const API_BASE_URL =
+  "http://emedicine-api.runasp.net/api";
+
+export const ENV = {
+  IMAGE_BASE_URL:
+    "http://emedicine-api.runasp.net",
 };
