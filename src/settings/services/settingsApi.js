711a2861af04290eb7ec3e39580a27f357d36174
bmtiwari9450 <balmukund.tiwari@mydesq.com>
2026-06-20 23:32:13 +0530

    corrected url

--- src/settings/services/settingsApi.js
+++ src/settings/services/settingsApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://localhost:44302/api";
+const BASE_URL = "https://emedicine-api.runasp.net/api";
 
 // Notification
 export const getNotificationSettings = async () => {
